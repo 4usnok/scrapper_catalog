83dbbe3ea7d6b1636200a292f864fecd09d499a0
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-12-29 04:45:03 +0300

    commit from scrap

--- src/convert_to_xlsx.py
+++ src/convert_to_xlsx.py
@@ -9,10 +9,11 @@ from src.config_db import engine, table_name
 class SaveData:
 
     def __init__(self):
-        self.result_title = []
-        self.result_href = []
+        self.all_products = []
         self.product_container = None
-        self.card_titles = None
+        self.card_models = None
+        self.old_price = None
+        self.new_price = None
         self.soup = None
         self.df = None
 
@@ -22,16 +23,45 @@ class SaveData:
             html_content = f.read()
         self.soup = BeautifulSoup(html_content, features="html.parser")
         self.product_container = self.soup.find("div", class_="product-card-list")
-        self.card_titles = self.product_container.find_all(
+        self.card_models = self.product_container.find_all(
             "a", class_="product-card__link j-card-link j-open-full-product-card"
+        )
+        self.old_price = self.product_container.find_all("del")
+        self.new_price = self.product_container.find_all(
+            "ins", class_="price__lower-price wallet-price red-price"
         )
         logging.info("Чтение html-файла прошло успешно")
 
     def pars_model(self):
-        """Парсинг названий и ссылок моделей"""
-        for info_model in self.card_titles:
-            self.result_title.append(info_model["aria-label"])
-            self.result_href.append(info_model["href"])
+        """Парсинг информации моделей"""
+        for info_model, old, new in zip(
+            self.card_models, self.old_price, self.new_price
+        ):
+            # Посчитаем скидку
+            price_old_str = old.text.replace("\xa0", "")
+            price_old_str = price_old_str.replace(" ", "")
+            price_old_str = price_old_str.replace("₽", "")
+            price_old_int = int(price_old_str)
+            price_new_str = new.text.replace("\xa0", "")
+            price_new_str = price_new_str.replace(" ", "")
+            price_new_str = price_new_str.replace("₽", "")
+            price_new_int = int(price_new_str)
+            discount_price = (price_old_int - price_new_int) // 100
+            if 0 > discount_price:
+                discount_price = f"Цена увеличилась на {discount_price * -1}"
+            elif discount_price == 0:
+                discount_price = "Стоимость не изменилась"
+            else:
+                discount_price = f"Цена уменьшилась на {discount_price}"
+
+            result_list = {
+                "title_model": info_model["aria-label"],
+                "old_price": old.text.replace("\xa0", ""),
+                "new_price": new.text.replace("\xa0", ""),
+                "discount_price": f"{discount_price}%",
+                "url_model": info_model["href"],
+            }
+            self.all_products.append(result_list)
 
     def convert_to_file(self):
         """Сохранение в файл xlsx"""
@@ -42,12 +72,7 @@ class SaveData:
         # Даём название странице
         sheet.tittle = "Товары"
         # Записываем данные в ячейки
-        self.df = pd.DataFrame(
-            {
-                "title_model": self.result_title,
-                "url_model": self.result_href,
-            }
-        )
+        self.df = pd.DataFrame(self.all_products)
         self.df.to_excel("data/catalog.xlsx", index=False)
         logging.info("Создание файла xlxs с отфильтрованными товарами прошло успешно")
 
