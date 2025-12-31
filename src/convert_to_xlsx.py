3816215e682ce0fabc7e788ba6d3388069c3ffff
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-12-31 03:49:46 +0300

    commit from scrap

--- src/convert_to_xlsx.py
+++ src/convert_to_xlsx.py
@@ -1,6 +1,7 @@
 import logging
 import openpyxl
 import pandas as pd
+import pyshorteners
 from bs4 import BeautifulSoup
 
 from src.config_db import engine, table_name
@@ -9,13 +10,24 @@ from src.config_db import engine, table_name
 class SaveData:
 
     def __init__(self):
-        self.all_products = []
-        self.product_container = None
-        self.card_models = None
-        self.old_price = None
-        self.new_price = None
         self.soup = None
         self.df = None
+        self.product_container = None
+        self.card_models = None
+        self.name_model = None
+        self.old_price = None
+        self.new_price = None
+        self.card_brand = None
+        self.delivery_date = None
+        self.rating_model = None
+        self.original_model = None
+        self.number_of_reviews = None
+        self.discount_price = None
+
+        self.all_products = []
+        self.is_original = []
+        self.short_url = []
+        self.discount_list = []
 
     def read_file(self):
         """Чтение html файла"""
@@ -26,40 +38,89 @@ class SaveData:
         self.card_models = self.product_container.find_all(
             "a", class_="product-card__link j-card-link j-open-full-product-card"
         )
+        self.name_model = self.product_container.find_all(
+            "span", class_="product-card__name"
+        )
+
         self.old_price = self.product_container.find_all("del")
         self.new_price = self.product_container.find_all(
             "ins", class_="price__lower-price wallet-price red-price"
         )
+        self.card_brand = self.product_container.find_all(
+            "span", class_="product-card__brand"
+        )
+        self.delivery_date = self.product_container.find_all("span", class_="btn-text")
+        self.rating_model = self.product_container.find_all(
+            "span", class_="address-rate-mini address-rate-mini--sm"
+        )
+        self.original_model = self.product_container.find_all(
+            "span",
+            class_="product-card__original-mark icon-original-check originalMark--b3N5n",
+        )
+        self.number_of_reviews = self.product_container.find_all(
+            "span", class_="product-card__count"
+        )
         logging.info("Чтение html-файла прошло успешно")
 
-    def pars_model(self):
-        """Парсинг информации моделей"""
-        for info_model, old, new in zip(
-            self.card_models, self.old_price, self.new_price
-        ):
-            # Посчитаем скидку
-            price_old_str = old.text.replace("\xa0", "")
-            price_old_str = price_old_str.replace(" ", "")
-            price_old_str = price_old_str.replace("₽", "")
-            price_old_int = int(price_old_str)
-            price_new_str = new.text.replace("\xa0", "")
-            price_new_str = price_new_str.replace(" ", "")
-            price_new_str = price_new_str.replace("₽", "")
-            price_new_int = int(price_new_str)
-            discount_price = (price_old_int - price_new_int) // 100
-            if 0 > discount_price:
-                discount_price = f"Цена увеличилась на {discount_price * -1}"
-            elif discount_price == 0:
-                discount_price = "Стоимость не изменилась"
+    def discount_calc(self):
+        """Расчет скидки"""
+        for old, new in zip(self.old_price, self.new_price):
+            price_old_str, price_new_str = old.text.replace(
+                "\xa0", ""
+            ), new.text.replace("\xa0", "")
+            price_old_str, price_new_str = price_old_str.replace(
+                " ", ""
+            ), price_new_str.replace(" ", "")
+            price_old_str, price_new_str = price_old_str.replace(
+                "₽", ""
+            ), price_new_str.replace("₽", "")
+            price_old_int, price_new_int = int(price_old_str), int(price_new_str)
+            if price_old_int > 0:
+                self.discount_price = (
+                    (price_old_int - price_new_int) / price_old_int
+                ) * 100
             else:
-                discount_price = f"Цена уменьшилась на {discount_price}"
+                self.discount_price = 0
+            if 0 > self.discount_price:
+                self.discount_list.append(
+                    f"Товар подорожал на {int(self.discount_price) * -1}%"
+                )
+            elif self.discount_price == 0:
+                self.discount_list.append("Стоимость не изменилась")
+            else:
+                self.discount_list.append(
+                    f"Товар подешевел на {int(self.discount_price)}%"
+                )
 
+    def shorten_url(self):
+        """Сокращатель ссылок"""
+        for url in self.card_models:
+            original_href = url["href"]
+            self.short_url.append(pyshorteners.Shortener().clckru.short(original_href))
+
+    def pars_model(self):
+        """Сбор информации карточки товара"""
+        for name, brand, old, new, disc, url, delivery, rating, rev in zip(
+            self.name_model,
+            self.card_brand,
+            self.old_price,
+            self.new_price,
+            self.discount_list,
+            self.short_url,
+            self.delivery_date,
+            self.rating_model,
+            self.number_of_reviews,
+        ):
             result_list = {
-                "title_model": info_model["aria-label"],
+                "title_model": name.text,
+                "brand_model": brand.text,
                 "old_price": old.text.replace("\xa0", ""),
                 "new_price": new.text.replace("\xa0", ""),
-                "discount_price": f"{discount_price}%",
-                "url_model": info_model["href"],
+                "discount_price": f"{disc}",
+                "url_model": url,
+                "delivery_date": delivery.text,
+                "rating": rating.text,
+                "number_of_rev": rev.text,
             }
             self.all_products.append(result_list)
 
@@ -94,6 +155,8 @@ class SaveData:
 def main_convert():
     pars = SaveData()
     pars.read_file()
+    pars.discount_calc()
+    pars.shorten_url()
     pars.pars_model()
     pars.convert_to_file()
     pars.db_work()
